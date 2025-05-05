674c50657b5105747d13c6454956de6c979f952b
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2025-05-05 16:02:36 +0000

    Add reference

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+set -e
+
 for method in OPENMP OPENMPSINGLECOLUMN OPENACCSINGLECOLUMN
 do
   cp lparallelmethod.txt.$method lparallelmethod.txt
   ./main.x < input.txt > output.txt.$method
+  diff output.txt.$method ref/output.txt.$method
 done
 
